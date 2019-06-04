231be533e64bdf150fd12b6cacf6685b1a856b52
Adi Kantawala <adikan@ucla.edu>
2019-06-04 11:46:00 -0700

    .

--- wordLadder.js
+++ wordLadder.js
@@ -45,3 +45,35 @@ var ladderLength = function(start, end, list) {
     }
     return 0;
 };
+
+//another way to do it
+function ladderLength(beginWord, endWord, wordList) {
+  let len = 1;
+  let queue = [beginWord];
+  const dict = new Set(wordList);
+  const seen = new Set(queue);
+  
+  while (queue.length) {
+      let next = [];
+      for(let v of queue){
+          if(v === endWord) return len
+
+          const arr = v.split('');
+          for (let i = 0; i < arr.length; i++) {
+              for (let d = 0; d < 26; d++) {
+                  arr[i] = String.fromCharCode(97+d);
+                  const nv = arr.join('');
+                  if (!seen.has(nv) && dict.has(nv)) {
+                      next.push(nv);
+                      seen.add(nv);
+                  }
+                  arr[i] = v[i];
+              }
+          }
+      }
+      queue = next;
+      len +=1
+  }
+
+  return 0;
+}
