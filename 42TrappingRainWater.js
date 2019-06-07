00f61453542109d9b408738ee884a9a8342de9d7
Adi Kantawala <adikan@ucla.edu>
2019-06-07 13:17:35 -0700

    .

---
+++ 42TrappingRainWater.js
@@ -0,0 +1,7 @@
+//https://leetcode.com/problems/trapping-rain-water/
+
+//answer below
+//https://leetcode.com/problems/trapping-rain-water/discuss/183237/Another-JavaScript-solution-(99)-w-bonus-illustration
+
+
+// basic approach is to move toward the peak while keeping an accumulator to keep track of the amount of water.
